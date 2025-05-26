aa4957063742061187d8386d1124be71499409a6
quacumque <quacumque@fastmail.jp>
2025-05-26 15:56:09 +0900

    build: rootless Dockerfile; fix git sha in version
    Signed-off-by: quacumque <quacumque@fastmail.jp>

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,18 @@ FROM rust:alpine3.21 AS builder
 
 WORKDIR /app
 
-RUN apk add musl-dev pkgconfig openssl-dev openssl-libs-static
-
-# NOTE: this disregards `./rust-toolchain.toml`, but it's fine
-COPY Cargo.lock Cargo.toml build.rs ./
+RUN apk add build-base pkgconfig openssl-dev openssl-libs-static git
+COPY Cargo* rust-toolchain.toml build.rs ./
 COPY src src
-RUN cargo fetch
+COPY .git .git
+RUN cargo fetch --locked
 RUN cargo build --release
 
 FROM alpine:3.21
+
 COPY --from=builder /app/target/release/iroha_explorer /usr/local/bin/
+
+RUN adduser --disabled-password --gecos '' explorer
+USER explorer
+
 CMD iroha_explorer serve
--- build.rs
+++ build.rs
@@ -1,7 +1,16 @@
+fn emit_git_sha() {
+    let output = std::process::Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output()
+        .expect("build script is executed with `git` installed and `.git` available");
+
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=VERGEN_GIT_SHA={}", git_hash.trim());
+
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/refs/heads/");
+}
+
 fn main() {
-    vergen::EmitBuilder::builder()
-        .git_sha(true)
-        .cargo_features()
-        .emit()
-        .unwrap();
+    emit_git_sha();
 }
